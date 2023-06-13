70c1b9f627f5ba33c218b68ac79e0d6c1f65ad66
johndward01 <johndward01@gmail.com>
2023-06-12 21:00:15 -0500

    added TODO's

--- Inheritance_Exercise_01/Program.cs
+++ Inheritance_Exercise_01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// TODO: Create a base class LibraryItem
+// TODO: Create 3 properties Title, SerialNumber, IsCheckedOut
+// TODO: Create 2 methods CheckOut() and Return() that change the status of IsCheckedOut
+
+// TODO: Create 2 derived classes Book and DVD
+// TODO: Give them both 2 unique properties (example: Author and Director)
+
+// TODO: Create a List<LibraryItem> and add 2 books and 2 dvd instances to it (fully filled out)
+
+// TODO: Display all of the items creatively to the console
+
